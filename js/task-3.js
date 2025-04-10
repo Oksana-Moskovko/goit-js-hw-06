3f1ebca45474539976d6409a0e7d57b2e77536ab
Oksana <130862204+Oksana-Moskovko@users.noreply.github.com>
2025-04-11 00:16:13 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,27 @@
-function getElementWidth(content, padding, border) {
-    return Number.parseFloat(content) +  (Number.parseFloat(padding) +  Number.parseFloat(border)) *2;
-};
+class StringBuilder {
+  #value 
+  
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+  
+  getValue() {
+  return this.#value 
+  }
+  
+  padEnd(str) {
+    this.#value = this.#value + str
+  }
+    
+  padStart(str) {
+    this.#value = str + this.#value 
+  }
+    
+  padBoth(str) {
+    this.padStart(str);
+    this.padEnd(str);
+  }
+}
 
 const builder = new StringBuilder(".");
 console.log(builder.getValue()); // "."
